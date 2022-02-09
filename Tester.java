f6fdc5e13b80c2dc83673caf2f2af0d7f1d65028
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-08 23:30:06 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,1 +1,12 @@
-
+public class Tester
+{
+  public static void main(String[] args)
+  {
+    Person p1 = new Person("Kevin", 2004);
+    Person p2 = new Person("Jacky", 2003);
+    Person p3 = new Instructor("Garten", 1960, 400);
+    Person p4 = new Instructor("Torline", 1990, 300);
+    Person p5 = new Scholar("Sebastian", 2004, "Mechanical Engineering");
+    Person p6 = new Scholar("Santiago", 2004, "Mechanical Engineering");
+  }
+}
