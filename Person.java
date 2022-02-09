f7c999b204c4c23c9ec13ed1d821ad07ac81a1ef
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-08 23:18:52 -0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+  private String name;
+  private int year;
+  public Person(String name, int year)
+  {
+    this.name = name;
+    this.year = year;
+  }
+  public String toString()
+  {
+    return("The name is" + name + year);
+  }
+}
