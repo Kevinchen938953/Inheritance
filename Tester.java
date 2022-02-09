333e054a4412f16d0986e1a71ea0d5b0b3badbd5
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-09 08:12:46 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -3,10 +3,16 @@ public class Tester
   public static void main(String[] args)
   {
     Person p1 = new Person("Kevin", 2004);
+    System.out.println(p1);
     Person p2 = new Person("Jacky", 2003);
-    Person p3 = new Instructor("Garten", 1960, 400);
-    Person p4 = new Instructor("Torline", 1990, 300);
-    Person p5 = new Scholar("Sebastian", 2004, "Mechanical Engineering");
-    Person p6 = new Scholar("Santiago", 2004, "Mechanical Engineering");
+    System.out.println(p2);
+    Instructor p3 = new Instructor("Garten", 1960, 400);
+    System.out.println(p3);
+    Instructor p4 = new Instructor("Torline", 1990, 300);
+    System.out.println(p4);
+    Scholar p5 = new Scholar("Sebastian", 2004, "Mechanical Engineering");
+    System.out.println(p5);
+    Scholar p6 = new Scholar("Santiago", 2004, "Mechanical Engineering");
+    System.out.println(p6);
   }
 }
